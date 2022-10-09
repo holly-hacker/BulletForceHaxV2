a9145a1695146d42500ebeba3a76daec379aa3f2
HoLLy <dario.db@hotmail.com>
2022-10-09 22:19:29 +0200

    Add UI scale option

--- bulletforcehax2_ui/src/lib.rs
+++ bulletforcehax2_ui/src/lib.rs
@@ -5,20 +5,27 @@ use futures_util::lock::Mutex;
 
 pub struct BulletForceHaxMenu {
     hax: Arc<Mutex<HaxState>>,
+    first_frame: bool,
 }
 
 impl BulletForceHaxMenu {
     pub fn new(hax: Arc<Mutex<HaxState>>) -> Self {
-        Self { hax }
+        Self {
+            hax,
+            first_frame: true,
+        }
     }
 
     pub fn update(&mut self, ctx: &egui::Context) {
+        if self.first_frame {
+            ctx.set_pixels_per_point(1.5f32);
+            self.first_frame = false;
+        }
+
         // set framerate to 20fps
         ctx.request_repaint_after(std::time::Duration::from_secs_f32(1f32 / 10f32));
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("hax menu goes here :D");
-
             let mut hax = futures::executor::block_on(self.hax.lock());
 
             ui.heading("Info");
@@ -37,6 +44,28 @@ impl BulletForceHaxMenu {
                 "Show games for other versions",
             );
             ui.checkbox(&mut hax.strip_passwords, "Strip passwords");
+            ui.add_space(16f32);
+
+            ui.heading("UI");
+            ui.horizontal(|ui| {
+                let scale = ctx.pixels_per_point();
+                ui.label(format!("Zoom: {scale}"));
+
+                if ui.button("-").clicked() {
+                    if scale > 2f32 {
+                        ctx.set_pixels_per_point(scale - 1f32);
+                    } else if scale > 1f32 {
+                        ctx.set_pixels_per_point(scale - 0.5f32);
+                    }
+                }
+                if ui.button("+").clicked() {
+                    if scale < 2f32 {
+                        ctx.set_pixels_per_point(scale + 0.5f32);
+                    } else {
+                        ctx.set_pixels_per_point(scale + 1f32);
+                    }
+                }
+            });
             ui.add_space(16f32);
 
             #[cfg(debug_assertions)]
