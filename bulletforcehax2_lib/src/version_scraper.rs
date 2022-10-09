a9e8d3421685e2a40493ef0f2d904427234c885e
HoLLy <dario.db@hotmail.com>
2022-10-09 20:13:22 +0200

    Remove ureq dependency from scraper, improve logging
    Dependencies down from 287 to 275

--- bulletforcehax2_lib/src/version_scraper.rs
+++ bulletforcehax2_lib/src/version_scraper.rs
@@ -1,6 +1,9 @@
 //! Provides a method to download the latest version of the game files.
 
-use std::sync::mpsc::{channel, Receiver, Sender};
+use std::time::{Duration, Instant};
+
+use hyper::{body::HttpBody, Body, Client, Request};
+use tokio::sync::mpsc::{channel, Receiver, Sender};
 
 use anyhow::{anyhow, Context, Result};
 use tracing::debug;
@@ -43,44 +46,49 @@ pub enum ProgressReport {
 
 /// Starts a thread that scrapes and downloads the game files. It returns a receiver that produces progress info.
 pub fn start_download_thread() -> Result<Receiver<ProgressReport>> {
-    let (tx, rx) = channel();
+    let (tx, rx) = channel(128);
 
-    std::thread::spawn(move || {
-        if let Err(e) = do_download(tx.clone()) {
-            _ = tx.send(ProgressReport::Crashed(e.to_string()));
+    tokio::spawn(async move {
+        let tx = tx.clone();
+        if let Err(e) = do_download(tx.clone()).await {
+            tracing::error!("Error while downloading version: {e}");
+            _ = tx.send(ProgressReport::Crashed(e.to_string())).await;
         }
     });
 
     Ok(rx)
 }
 
-fn do_download(tx: Sender<ProgressReport>) -> Result<()> {
+async fn do_download(tx: Sender<ProgressReport>) -> Result<()> {
+    // TODO: add tower-http's compressing middleware
+    let client = Client::builder().build::<_, hyper::Body>(hyper_tls::HttpsConnector::new());
+
     // download loader
     download_file_with_progress(
+        &client,
         UNITY_LOADER_URL,
         "UnityLoader.js",
         FileType::UnityLoader,
         tx.clone(),
-    )?;
+    )
+    .await?;
 
     // find game files
-    let source_1 = ureq::get(POKI_URL).call()?.into_string()?;
+    let source_1 = hyper_get(&client, POKI_URL).await.context("get source_1")?;
     let match_1 = regex::Regex::new(POKI_FRAME_URL_PATTERN)?
         .find(&source_1)
         .ok_or_else(|| anyhow!("Could not find poki regex 1"))?;
     let frame_url = match_1.as_str();
-    let source_2 = ureq::get(frame_url)
-        .set("referer", "https://poki.com")
-        .call()?
-        .into_string()?;
+    let source_2 = hyper_get_with_referrer(&client, frame_url)
+        .await
+        .context("get source_2")?;
     let match_2 = regex::Regex::new(POKI_GAME_URL_PATTERN)?
         .find(&source_2)
         .ok_or_else(|| anyhow!("Could not find poki regex 2"))?;
     let game_url = match_2.as_str();
-    let source_3 = ureq::get(game_url)
-        .set("referer", "https://poki.com")
-        .call()?
-        .into_string()?;
+    let source_3 = hyper_get_with_referrer(&client, game_url)
+        .await
+        .context("get source_3")?;
     let match_3 = regex::Regex::new(POKI_GAME_JSON_PATTERN)?
         .captures(&source_3)
         .ok_or_else(|| anyhow!("Could not find poki regex 3"))?
@@ -88,15 +96,23 @@ fn do_download(tx: Sender<ProgressReport>) -> Result<()> {
         .ok_or_else(|| anyhow!("Could not find group in poki regex 3"))?;
     let rel_url_json = match_3.as_str().replace("\\/", "/");
 
-    let abs_url_json_base = dbg!(&game_url[..game_url.rfind('/').unwrap()]);
+    let abs_url_json_base = &game_url[..game_url.rfind('/').unwrap()];
     let abs_url_json = abs_url_json_base.to_string() + "/" + &rel_url_json;
-    download_file_with_progress(&abs_url_json, &rel_url_json, FileType::GameJson, tx.clone())?;
+    download_file_with_progress(
+        &client,
+        &abs_url_json,
+        &rel_url_json,
+        FileType::GameJson,
+        tx.clone(),
+    )
+    .await?;
 
     // yes I'm downloading the json twice, I cba to rewrite the code
-    let json = ureq::get(&abs_url_json).call()?.into_string()?;
+    let json = hyper_get(&client, &abs_url_json).await?;
     let json: serde_json::Value = serde_json::from_str(&json).context("parse game json")?;
 
-    let base_url_file = dbg!(&abs_url_json[..abs_url_json.rfind('/').unwrap()]);
+    let base_url_file = &abs_url_json[..abs_url_json.rfind('/').unwrap()];
+    // TODO: can happen in parallel
     for rel_url in [
         json["dataUrl"]
             .as_str()
@@ -110,28 +126,65 @@ fn do_download(tx: Sender<ProgressReport>) -> Result<()> {
     ] {
         let abs_url_file = format!("{base_url_file}/{rel_url}");
         let file_name = &abs_url_file[(abs_url_json_base.len() + 1)..];
-        download_file_with_progress(&abs_url_file, file_name, FileType::GameFile, tx.clone())?;
+        download_file_with_progress(
+            &client,
+            &abs_url_file,
+            file_name,
+            FileType::GameFile,
+            tx.clone(),
+        )
+        .await?;
     }
 
-    tx.send(ProgressReport::AllFilesDownloaded)?;
+    tx.send(ProgressReport::AllFilesDownloaded)
+        .await
+        .ok()
+        .context("all files downloaded send")
+        .unwrap();
 
     Ok(())
 }
 
-fn download_file_with_progress(
+async fn hyper_get<T>(client: &Client<T>, url: &str) -> Result<String>
+where
+    T: hyper::client::connect::Connect + Clone + Send + Sync + 'static,
+{
+    let uri = hyper::Uri::try_from(url)?;
+    let response = client.get(uri).await?;
+    let bytes = hyper::body::to_bytes(response).await?;
+    Ok(String::from_utf8(bytes.to_vec())?)
+}
+
+async fn hyper_get_with_referrer<T>(client: &Client<T>, url: &str) -> Result<String>
+where
+    T: hyper::client::connect::Connect + Clone + Send + Sync + 'static,
+{
+    let uri = hyper::Uri::try_from(url)?;
+    let request = Request::builder()
+        .header("referer", "https://poki.com")
+        .uri(uri)
+        .body(Body::empty())?;
+    let response = client.request(request).await?;
+    let bytes = hyper::body::to_bytes(response).await?;
+    Ok(String::from_utf8(bytes.to_vec())?)
+}
+
+async fn download_file_with_progress<T>(
+    client: &Client<T>,
     url: &str,
     name: &str,
     file_type: FileType,
     tx: Sender<ProgressReport>,
-) -> Result<()> {
-    let response = ureq::get(url).call()?;
+) -> Result<()>
+where
+    T: hyper::client::connect::Connect + Clone + Send + Sync + 'static,
+{
+    let mut response = client.get(hyper::Uri::try_from(url).unwrap()).await?;
     let content_length = response
-        .header("content-length")
-        .and_then(|len| len.parse::<u64>().ok());
+        .headers()
+        .get("content-length")
+        .and_then(|len| len.to_str().unwrap().parse::<u64>().ok());
 
-    use std::io::Read;
-
-    let mut reader = response.into_reader();
     let mut data = vec![];
 
     tx.send(ProgressReport::Progress {
@@ -139,28 +192,56 @@ fn download_file_with_progress(
         name: name.to_string(),
         downloaded: 0,
         total: content_length,
-    })?;
-    let mut buffer = [0u8; 1024 * 256];
-    loop {
-        let len = reader.read(&mut buffer)?;
-        data.extend_from_slice(&buffer[..len]);
+    })
+    .await
+    .ok()
+    .context("initial progress send")
+    .unwrap();
 
-        if len != 0 {
+    const UPDATE_FREQUENCY: Duration = Duration::from_millis(50);
+    let mut last_progress = Instant::now();
+    while let Some(next) = response.data().await {
+        let next = next?;
+        data.extend_from_slice(&next);
+
+        let now = Instant::now();
+        if (now - last_progress) > UPDATE_FREQUENCY {
             tx.send(ProgressReport::Progress {
                 file_type,
                 name: name.to_string(),
                 downloaded: data.len() as u64,
                 total: content_length,
-            })?;
-        } else {
-            tx.send(ProgressReport::FileDownloaded {
-                file_type,
-                name: name.to_string(),
-                data,
-            })?;
-            break;
+            })
+            .await
+            .ok()
+            .context("progress send")
+            .unwrap();
+
+            last_progress = now;
         }
     }
+
+    // kind of a hack
+    tx.send(ProgressReport::Progress {
+        file_type,
+        name: name.to_string(),
+        downloaded: data.len() as u64,
+        total: content_length,
+    })
+    .await
+    .ok()
+    .context("final progress send")
+    .unwrap();
+
+    tx.send(ProgressReport::FileDownloaded {
+        file_type,
+        name: name.to_string(),
+        data,
+    })
+    .await
+    .ok()
+    .context("file downloaded send")
+    .unwrap();
 
     debug!("Downloaded {name} from {url}");
     Ok(())
