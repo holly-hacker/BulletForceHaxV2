6a4590df4c35f41f8ce1aa61493941659234223e
HoLLy <dario.db@hotmail.com>
2022-10-13 15:52:49 +0200

    Add json logging to file

--- bulletforcehax2_app/src/main.rs
+++ bulletforcehax2_app/src/main.rs
@@ -12,7 +12,6 @@ use bulletforcehax2_ui::BulletForceHaxMenu;
 use once_cell::sync::OnceCell;
 use tao_egui::WindowCreationSettings;
 use tracing::{debug, error, info};
-use tracing_subscriber::prelude::*;
 use version_manager::{VersionConfig, VersionManager};
 use wry::{
     application::{
@@ -89,27 +88,7 @@ async fn main() {
 
 async fn real_main() -> anyhow::Result<()> {
     // initialize logging
-    let default_logging_level = tracing::Level::DEBUG;
-    let subscriber = tracing_subscriber::fmt()
-        .with_max_level(default_logging_level)
-        .with_timer(tracing_subscriber::fmt::time::uptime())
-        .finish();
-
-    let filter = tracing_subscriber::filter::Targets::new()
-        .with_target("app", default_logging_level)
-        .with_target("bulletforcehax2_lib", default_logging_level)
-        .with_target("bulletforcehax2_ui", default_logging_level);
-
-    subscriber.with(filter).init();
-
-    #[cfg(debug_assertions)]
-    {
-        tracing::trace!("trace enabled");
-        tracing::debug!("debug enabled");
-        tracing::info!("info enabled");
-        tracing::warn!("warn enabled");
-        tracing::error!("error enabled");
-    }
+    let _guard = init_logging();
 
     // version manager init
     let version_manager = VersionManager::new(Path::new("bfhax_data")).unwrap();
@@ -215,4 +194,80 @@ async fn real_main() -> anyhow::Result<()> {
             _ => (),
         }
     });
+}
+
+fn init_logging() -> tracing_appender::non_blocking::WorkerGuard {
+    use tracing::{level_filters::LevelFilter, Level};
+    use tracing_subscriber::prelude::*;
+
+    let logging_level_console = cfg!(debug_assertions)
+        .then_some(Level::DEBUG)
+        .unwrap_or(Level::INFO);
+    let logging_level_file = cfg!(debug_assertions)
+        .then_some(Level::TRACE)
+        .unwrap_or(Level::DEBUG);
+
+    let filter = tracing_subscriber::filter::Targets::new()
+        .with_target("app", Level::TRACE)
+        .with_target("bulletforcehax2_lib", Level::TRACE)
+        .with_target("bulletforcehax2_ui", Level::TRACE);
+
+    let console_layer = {
+        tracing_subscriber::fmt::layer()
+            .with_writer(std::io::stdout)
+            .compact()
+            .with_filter(filter.clone())
+            .with_filter(LevelFilter::from_level(logging_level_console))
+    };
+
+    // file logs
+    let (file_layer, guard) = {
+        use time::OffsetDateTime;
+        let current_time = OffsetDateTime::now_local().unwrap_or(OffsetDateTime::now_utc());
+
+        let file_name = format!(
+            "log_{:04}{:02}{:02}_{:02}{:02}{:02}.json",
+            current_time.year(),
+            u8::from(current_time.month()),
+            current_time.day(),
+            current_time.hour(),
+            current_time.minute(),
+            current_time.second()
+        );
+
+        // we're using tracing_appender because it provides non-blocking logging
+        // just logging using std::fs::File may be enough, but has to be tested first.
+        let appender = tracing_appender::rolling::never("bfhax_data/logs", file_name);
+        let (non_blocking_appender, guard) = tracing_appender::non_blocking(appender);
+
+        let layer = tracing_subscriber::fmt::layer()
+            .with_writer(non_blocking_appender)
+            .json()
+            .with_filter(filter.clone())
+            .with_filter(LevelFilter::from_level(logging_level_file));
+
+        (layer, guard)
+    };
+
+    /*
+    let subscriber = tracing_subscriber::fmt()
+        .with_timer(tracing_subscriber::fmt::time::uptime())
+        .finish();
+    */
+    let subscriber = tracing_subscriber::registry();
+
+    let subscriber = subscriber.with(file_layer).with(console_layer);
+
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    #[cfg(debug_assertions)]
+    {
+        tracing::trace!("trace enabled");
+        tracing::debug!("debug enabled");
+        tracing::info!("info enabled");
+        tracing::warn!("warn enabled");
+        tracing::error!("error enabled");
+    }
+
+    guard
 }
